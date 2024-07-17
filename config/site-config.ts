c768cee08e9782ad7cc177b0771b3695530302fc
brandonhach <specialcambo@gmail.com>
2024-07-16 22:08:33 -0400

    changed pics

--- config/site-config.ts
+++ config/site-config.ts
@@ -74,14 +74,13 @@ const GridItems: GridItemInterface[] = [
 		video: 'drake-spotify.mp4',
 		link: 'https://open.spotify.com/user/1z1b1y7vu3j7ntroaf6c7ng84',
 	},
-
 	{
 		layout: '2x4',
 		type: 'languages',
 		title: 'Languages:',
 		title2: 'Frameworks:',
 		title3: 'DBMS',
-		image: 'jordan.jpg',
+		image: 'whiteferrari.jpg',
 		languages: [
 			{
 				title: 'Python',
@@ -168,12 +167,12 @@ const GridItems: GridItemInterface[] = [
 	{
 		layout: '2x2',
 		type: 'project',
-		title: 'Cheese Ecommerce',
+		title: 'repguard.id',
 		icon: 'github',
 		description:
-			'A luxury cheese website for sellers to list their expensive cheese for sale. And buyers can bid their cheese of choice.',
-		link: 'https://github.com/brandonhach/Luxury-Cheese-Marketplace',
-		image: 'cheese.png',
+			'Trust and security platform for the internet. Aim to help fight fraud online by our custom auth methods.',
+		link: 'https://github.com/brandonhach/rep',
+		image: 'rep.gif',
 		setBlur: true,
 	},
 	{
@@ -191,18 +190,15 @@ const GridItems: GridItemInterface[] = [
 		link: 'https://unsplash.com/photos/black-steering-wheel-in-close-up-photography-t1bJyq-Xfag',
 	},
 	{
-		layout: '1x2',
-		title: 'rolex',
-		type: 'picture',
-		image: 'rolex.png',
-		link: 'https://www.youtube.com/watch?v=Prtzy3hEJ2w',
-	},
-	{
-		layout: '1x2',
-		title: 'mclaren',
-		type: 'picture',
-		image: 'mclaren.jpg',
-		link: 'https://unsplash.com/photos/red-ferrari-458-italia-on-road-2ihYdRZgyWw',
+		layout: '2x2',
+		type: 'project',
+		title: 'Cheese Ecommerce',
+		icon: 'github',
+		description:
+			'A luxury cheese website for sellers to list their expensive cheese for sale. And buyers can bid their cheese of choice.',
+		link: 'https://github.com/brandonhach/Luxury-Cheese-Marketplace',
+		image: 'cheese.png',
+		setBlur: true,
 	},
 	{
 		layout: '2x2',
@@ -214,54 +210,59 @@ const GridItems: GridItemInterface[] = [
 		link: 'https://www.youtube.com/watch?v=MJbE3uWN9vE',
 	},
 	{
-		title: 'ferrari',
-		layout: '2x4',
-		type: 'picture',
-		video: 'ferrari-media.mp4',
-	},
-	{
-		layout: '2x4',
-		type: 'picture',
-		image: 'blue-jordans.jpg',
-	},
-	{
-		layout: '2x4',
-		type: 'picture',
-		image: 'city.jpg',
-		link: 'https://unsplash.com/photos/a-view-of-a-city-at-night-from-a-window-KVa4WFUiQk4',
-	},
-	{
-		layout: '2x4',
+		layout: '1x2',
 		type: 'picture',
 		image: 'buddha.jpg',
 	},
 
 	{
+		layout: '2x4',
+		title: 'lambo',
+		type: 'picture',
+		image: 'lambo.jpg',
+	},
+	{
+		layout: '1x2',
+		type: 'picture',
+		image: 'buddha.jpg',
+	},
+	{
+		layout: '1x2',
+		type: 'picture',
+		image: 'hanuman.jpg',
+	},
+	{
+		layout: '1x2',
+		title: 'hanuman2',
+		type: 'picture',
+		image: 'hanuman2.jpg',
+	},
+	{
+		layout: '1x2',
+		title: 'hanu',
+		type: 'picture',
+		image: 'hanu.jpg',
+	},
+	{
 		layout: '2x2',
-		title: 'charlotte',
-		type: 'picture',
-		image: 'charlotte.jpg',
-		link: 'https://unsplash.com/photos/aerial-photography-of-city-ABIBm48VYfQ',
+		type: 'social',
+		title: 'Dazegxd - Love Roulette',
+		icon: 'music',
+
+		image: 'race.gif',
+		link: 'https://www.youtube.com/watch?v=AVGNay892cI',
 	},
 	{
 		layout: '1x2',
-		title: 'jewelry',
+		title: 'hanu2',
 		type: 'picture',
-		image: 'jewelry.png',
-		link: 'https://www.youtube.com/watch?v=WqfZB_bKdRI',
-	},
-	{
-		layout: '1x2',
-		title: 'vlone',
-		type: 'picture',
-		image: 'vlone.png',
-		link: 'https://www.youtube.com/watch?v=haUi0A4WYqo',
+		image: 'hanu2.jpg',
 	},
 ];
 export const siteConfig = {
 	creator: 'Brandon Hach',
 	title: 'CS Alumni @ UNC Charlotte',
-	bio: 'Building websites and learning new technology everyday.',
+	bio: 'Building websites and learning new technology. 🇺🇸 🇰🇭 ',
 	location: 'Charlotte, NC',
 	locationLink:
 		'https://www.google.com/maps/place/Charlotte,+NC/@35.2027068,-81.1694775,10z/data=!3m1!4b1!4m6!3m5!1s0x88541fc4fc381a81:0x884650e6bf43d164!8m2!3d35.2270869!4d-80.8431267!16zL20vMGZzYjg?entry=ttu',
